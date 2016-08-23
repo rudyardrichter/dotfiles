a81d75910af1ec26f52ba75cd1e1b17322875d45
rudyardrichter <rudyardrichter@uchicago.edu>
2016-08-23 16:58:21 -0500

    Pare down zsh/lib

--- zsh/lib/abbreviations.zsh
+++ zsh/lib/abbreviations.zsh
@@ -24,8 +24,8 @@ no-magic-abbrev-expand() {
     LBUFFER+=' '
 }
 
-zle -N        magic-abbrev-expand
-zle -N        no-magic-abbrev-expand
-bindkey " "   magic-abbrev-expand
-bindkey "^x " no-magic-abbrev-expand
-bindkey -M isearch " " self-insert
+zle -N magic-abbrev-expand
+zle -N no-magic-abbrev-expand
+bindkey ' ' magic-abbrev-expand
+bindkey '^x ' no-magic-abbrev-expand
+bindkey -M isearch ' ' self-insert
