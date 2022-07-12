9279b97cda857ab7ca23cac72102bcd445f706de
Rudyard Richter <rudyardrichter@uchicago.edu>
2022-07-12 10:22:30 -0500

    Add gruvbox dircolors

--- zsh/themes/gruvbox.zsh
+++ zsh/themes/gruvbox.zsh
@@ -11,7 +11,7 @@ export LESS_TERMCAP_ue=$(printf "\e[33m")
 export LESS_TERMCAP_us=$(printf "\e[1;31m")
 
 # ls colors & tab completion colors
-eval `dircolors $ZSH/colors/dircolors.ansi-dark`
+eval `dircolors $ZSH/colors/gruvbox_dircolors`
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
 setopt prompt_subst
